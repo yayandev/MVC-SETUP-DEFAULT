bf9c1845c54a3f6af26009cf07e4c27ead4c7b51
Yayanfr20 <yayanfathurohman20@gmail.com>
2022-09-18 13:33:41 +0700

    Up

---
+++ app/config/config.php
@@ -0,0 +1,6 @@
+<?php
+define('BASEURL', 'http://localhost/RA3-UPG/public');
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+define('DB_NAME', 'ra3-upg');
---
+++ app/views/templates/header.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>RA3-UPG | <?= $data['judul']; ?></title>
+
+    <link rel="stylesheet" href="<?= BASEURL; ?>/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
+
+    <link rel="shortcut icon" href="<?= BASEURL; ?>/img/ico.png" type="images/x-icon">
+</head>
+
+<body id="con">
